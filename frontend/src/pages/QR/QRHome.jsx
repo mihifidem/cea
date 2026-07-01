2083cebb8807c05f619047bbd9f53f7104077384
mihifidem <mihifidem@gmail.com>
2026-07-01 13:59:32 +0200

    feat: h2

--- frontend/src/pages/QR/QRHome.jsx
+++ frontend/src/pages/QR/QRHome.jsx
@@ -3,6 +3,7 @@ export default function QRHome(){
     return( 
     <div className="container mt-5"> 
     <h1>Sistema QR</h1> 
+    <h2>Hola k ase</h2>
     <p> Escanea un código QR para acceder a un recurso. </p>
     <p> Para escanear un código QR, haz clic en el botón de abajo y permite el acceso a la cámara de tu dispositivo.    </p>
     <QRButton /> </div> ) }
